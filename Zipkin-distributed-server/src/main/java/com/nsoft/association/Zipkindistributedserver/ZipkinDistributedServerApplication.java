e4c4e52a2cb7c361403aace982799532b15fd6c1
Radhadatta <loving2datta@gmail.com>
2020-04-11 12:15:33 +0530

    Initial Zipkin local commit

---
+++ Zipkin-distributed-server/src/main/java/com/nsoft/association/Zipkindistributedserver/ZipkinDistributedServerApplication.java
@@ -0,0 +1,13 @@
+package com.nsoft.association.Zipkindistributedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinDistributedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedServerApplication.class, args);
+	}
+
+}
---
+++ Zipkin-distributed-server/src/test/java/com/nsoft/association/Zipkindistributedserver/ZipkinDistributedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsoft.association.Zipkindistributedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinDistributedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
